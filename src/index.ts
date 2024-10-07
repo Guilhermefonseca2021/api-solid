4748719690ef22712cfdea1465c3f31fea6397c2
Guilhermefonseca2021 <gsbloogs194@gmail.com>
2024-10-06 23:58:39 -0300

    docker init

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ app.listen({
     port: env.PORT,
     host: '0.0.0.0' // torna acessivel para o front-end
 }).then(() => {
-    console.log("🚀 HTTP Server Running@")
+    console.log("🚀 HTTP Server Running")
 })
